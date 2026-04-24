c2932b8b6d8cfa18c0d96cb7714bc77fd03fd2ed
Amiee <tjup02@gmail.com>
2026-04-24 22:44:11 +0800

    製作登入頁面

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
